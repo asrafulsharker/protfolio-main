fdcdef4c5c7e7c68c2a8a720fe8428ec3152e808
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2023-01-02 21:24:08 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Header from './components/header/header';
+import Home from './components/home/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <main className='main'>
+    <Home/>
+    </main>
+    </>
   );
 }
 
