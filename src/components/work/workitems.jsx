0adc275b1358e1040510dc8d1bc194324df05b60
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2023-01-09 15:58:09 +0600

    add

--- src/components/work/workitems.jsx
+++ src/components/work/workitems.jsx
@@ -6,7 +6,7 @@ const WorkItems = ({item}) => {
          <img src={item.image} alt="" className="work__img" />
          <h3 className="work__title">{item.title}</h3>
          <a href="#" className="work__button">
-          Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+          See Live <i className="bx bx-right-arrow-alt work__button-icon"></i>
          </a>
     </div>
   );
