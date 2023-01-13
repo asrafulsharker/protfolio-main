bb266cc020323f500f7af8860392e0e30d8b3dbf
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2023-01-13 15:50:03 +0600

    add

--- src/components/home/data.jsx
+++ src/components/home/data.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React,{ useRef, useEffect } from 'react';
+import Lottie from 'lottie-web';
 
-const data = () => {
+const Data = () => {
+  const container = useRef(null)
+
+  useEffect(() => {
+    Lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop : true,
+      autoplay: true,
+      animationData: require('./me_icon.json')
+    })
+  },[])
+
   return (
     <div className="home__data">
+      {/* .home__title-container */}
         <h1 className="home__title">Nirob
-        <svg
+
+        {/* <svg
                   width="36"
                   height="36"
                   viewBox="0 0 48 48"
@@ -52,10 +67,14 @@ const data = () => {
                     d="M46.2745 22.28C46.0495 21.83 45.7495 21.53 45.3745 21.23C45.4495 21.305 45.5245 21.38 45.5245 21.53C46.7995 24.08 42.6745 26.405 40.1995 29.405C38.1745 31.88 35.9245 33.83 34.7995 36.455C29.9995 47.93 16.0495 47.93 10.1995 42.68C15.5245 48.68 30.5245 49.28 35.5495 37.205C36.6745 34.58 38.9245 32.63 40.9495 30.155C43.4245 27.155 47.5495 24.755 46.2745 22.28ZM32.3245 28.13C27.4495 26.33 18.7495 29.63 19.9495 38.405C19.9495 30.23 27.3745 28.205 31.4245 28.205C32.0245 28.13 32.3245 28.13 32.3245 28.13Z"
                     fill="var(--container-color)"
                   ></path>
-                </svg>
+                </svg> */}
+               
         </h1>
+        <div className="container" ref={container}></div>
         <h3 className="home__subtitle">VisualDeveloper</h3>
-        <p className="home__description">I'm creative designer & developer based in BD, and I'm very passionate and dedicated team work.</p>
+        <p className="home__description">I have worked on several projects in the past half a year and gained expertise in frontend technologies such as HTML, CSS, Bootstrap, tailwind, JavaScript, and React JS, Firebase. I am also comfortable with the backend technologies like node.js, express, and MongoDB. I am also comfortable with Graphics Design.
+
+</p>
        
         <a href="#contact" className="button button--flex">
             Say Hello
@@ -82,4 +101,4 @@ const data = () => {
   )
 }
 
-export default data
+export default Data
