a9cc247080d8cc4ad49a3abc571db3163798340e
asrafulsharker <asrafulsharkernirob1376@gmail.com>
2024-08-09 21:17:01 +0600

    added link

--- src/components/home/social.jsx
+++ src/components/home/social.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import GoogleScholer from '../../assets/icon/googlescholar-svgrepo-com.svg';
+import Orcid from '../../assets/icon/orcid.svg';
+import Gmail from '../../assets/icon/icons8-gmail.svg';
 
 const Social = () => {
   return (
@@ -10,8 +13,16 @@ const Social = () => {
         <a href=" https://github.com/asrafulsharker" className="home__social-icon" target="_blank">
             <i class="uil uil-github-alt"></i>
         </a>
-        <a href=" https://github.com/asrafulsharker" className="home__social-icon" target="_blank">
-        <i class="fa-brands fa-google-scholar"></i>                </a>
+        <a href="https://scholar.google.com/citations?user=ul2pGgMAAAAJ&hl=en&authuser=2" className="home__social-icon" target="_blank">
+        <img src={GoogleScholer} alt="" className="home__social-icon"  style={{height:'20px'}}/>
+         </a>
+
+         <a href="https://orcid.org/0009-0004-2911-3890" className="home__social-icon" target="_blank">
+        <img src={Orcid} alt="" className="home__social-icon"  style={{height:'20px'}}/>
+         </a>
+         <a href="mailto:asrafulsharker.me@gmail.com" className="home__social-icon" target="_blank">
+         <img src={Gmail} alt="" className="home__social-icon"  style={{height:'20px'}}/>
+         </a>
     </div>
   )
 }
